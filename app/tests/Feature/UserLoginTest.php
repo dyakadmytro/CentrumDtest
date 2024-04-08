8b4b318a074346d4ae7d198aba38eea34f293a18
Dmitriy <neposydyakadmytro@gmail.com>
2024-04-08 15:34:52 +0300

    add login test

---
+++ app/tests/Feature/UserLoginTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserLoginTest extends TestCase
+{
+    use WithFaker;
+
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->user->delete();
+    }
+
+    public function test_user_success_login(): void
+    {
+        $this->get('/login')->assertOk();
+
+        $response = $this->post('/login', [
+            'login' => $this->user->email,
+            'password' => $this->user->password,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertLocation('home');
+    }
+}
