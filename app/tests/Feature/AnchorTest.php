2af0f6240fa86de5de33641abab8f0eff11715a8
Dmitriy <neposydyakadmytro@gmail.com>
2024-04-09 02:24:19 +0300

    update AnchorTest.php

--- app/tests/Feature/AnchorTest.php
+++ app/tests/Feature/AnchorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\Anchor;
 use App\Models\User;
+use App\Services\AnchorService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -28,15 +29,34 @@ class AnchorTest extends TestCase
     {
         $this->actingAs($this->user);
         $data = Anchor::factory()->make();
-//        $this->get(route('anchor.create'))->assertOk();
-        $response = $this->post(route('anchor.store'), [
-            $data->only(['title', 'url', 'ttl', 'max_follows'])
+        $this->get(route('anchor.create'))->assertOk();
+        $response = $this->post(route('anchor.store'), $data->only(['title', 'url', 'ttl', 'max_follows']));
+
+        $this->assertDatabaseHas('anchors', [
+            'url' => $data->url
         ]);
+        $response->assertRedirect(route('anchor.list'));
+    }
 
-//        $this->assertDatabaseHas('anchors', [
-//            'url' => $data->url
-//        ]);
-//        $response->assertRedirect(route('anchor.list'));
+    public function test_anchor_is_available(): void
+    {
+        $anchorService = $this->app->make(AnchorService::class);
+        $data = Anchor::factory()->make();
+        $anchor = $anchorService->create($data->only(['title', 'url', 'ttl', 'max_follows']));
 
+        $response = $this->get(route('slug', $anchor->slug));
+
+        $response->assertRedirect($anchor->url);
+    }
+
+    public function test_anchor_is_not_available(): void
+    {
+        $anchorService = $this->app->make(AnchorService::class);
+        $data = Anchor::factory()->make(['max_follows' => 1,'followed' => 2]);
+        $anchor = $anchorService->create($data->only(['title', 'url', 'ttl', 'max_follows', 'followed']));
+
+        $response = $this->get(route('slug', $anchor->slug));
+
+        $response->assertNotFound();
     }
 }
