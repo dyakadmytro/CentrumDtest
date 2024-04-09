04f313e17fbd3e3db0a5a633ca70c7f335733fa6
Dmitriy <neposydyakadmytro@gmail.com>
2024-04-09 19:23:09 +0300

    update layout structure

--- app/resources/views/components/layout.blade.php
+++ app/resources/views/components/layout.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    @yield('content')
-</div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-</html>
+@extends('components.base')
+
+@section('page')
+    <x-navbar/>
+    <div class="container">
+        @yield('content')
+    </div>
+@endsection
